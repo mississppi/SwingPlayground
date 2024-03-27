98f3ca7a8b0d3c2a883092e6ee63284df4d434ed
mississppi <msspstudio@gmail.com>
2024-03-27 23:27:53 +0900

    first commit

---
+++ App.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        JFrame f = new JFrame();
+        f.setSize(300, 200);
+        f.setVisible(true);
+    }
+}
