821e8ec1a96fa588ba55f214c0259aa94207817a
mississppi <msspstudio@gmail.com>
2024-03-28 23:53:45 +0900

    add close

--- App.java
+++ App.java
@@ -1,9 +1,14 @@
+import java.awt.*;
 import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        JFrame f = new JFrame();
-        f.setSize(300, 200);
-        f.setVisible(true);
+        JFrame frame = new JFrame("Swing");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setLayout(new BorderLayout());
+        frame.setSize(300, 200);
+        frame.setVisible(true);
     }
 }
